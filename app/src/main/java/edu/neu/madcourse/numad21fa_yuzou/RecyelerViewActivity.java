a8f4ec88a2ce5bcb5218bdc405c059cc8c70cabf
manazou <zou.yu1@northeastern.edu>
2022-02-11 00:33:02 -0500

    A4_Link_Collector

---
+++ app/src/main/java/edu/neu/madcourse/numad21fa_yuzou/RecyelerViewActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21fa_yuzou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecyelerViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recyelerview);
+    }
+}
