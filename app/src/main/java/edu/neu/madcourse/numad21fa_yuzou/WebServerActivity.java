820dcbec6c57fbeebee354fde523e34b79df386e
manazou <zou.yu1@northeastern.edu>
2022-03-03 07:20:52 -0500

    A6_At_Your_Service

--- app/src/main/java/edu/neu/madcourse/numad21fa_yuzou/WebServerActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_yuzou/WebServerActivity.java
@@ -1,35 +1,14 @@
 package edu.neu.madcourse.numad21fa_yuzou;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class WebServerActivity extends AppCompatActivity {
-    private static final String apikey = "046f38d91a8fb45bbbcbb710696b2b43";
-    private static final String TAG = "Find Movies";
-    private String searchTxt;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_service);
-
-        //EditText editSearch = findViewById(R.id.editTxt_movie_name);
-        //searchTxt = editSearch.getText().toString();
-
-
-
-        //Button button_imdb = findViewById(R.id.btn_imdb);
-        //Button button_mealdb = findViewById(R.id.btn_mealdb);
-
-        //button_imdb.setOnClickListener(new MyClickListener());
-
-
-
+        setContentView(R.layout.activity_web_server);
     }
-
-
-
 }
