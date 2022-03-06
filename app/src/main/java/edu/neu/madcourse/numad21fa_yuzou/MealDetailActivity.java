5c0d81d621b815451f40f648d0025b71467d2090
manazou <zou.yu1@northeastern.edu>
2022-03-06 12:01:36 -0500

    A6_At_Your_Service

--- app/src/main/java/edu/neu/madcourse/numad21fa_yuzou/MealDetailActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_yuzou/MealDetailActivity.java
@@ -13,47 +13,44 @@ import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 public class MealDetailActivity extends AppCompatActivity {
     private static final String TAG = "Meal Detail Activity";
+    private String id;
+    private TextView txt_detail;
+    private TextView txt_instruction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_detail);
 
-        TextView detail = findViewById(R.id.txtView_meal_detail);
-        TextView instruction = findViewById(R.id.txtView_meal_instruction);
-        ImageView image = findViewById(R.id.imageView_meal);
+        id = getIntent().getStringExtra("id");
 
-        instruction.setMovementMethod(ScrollingMovementMethod.getInstance());
-        detail.setText(getIntent().getStringExtra("meal"));
-        instruction.setText(getIntent().getStringExtra("instruction"));
-        Log.i(TAG, getIntent().getStringExtra("imagepath"));
-        //Drawable drawable = getDrawableFromURL(getIntent().getStringExtra("imagepath"));
+        txt_detail = findViewById(R.id.txtView_meal_detail);
+        txt_instruction = findViewById(R.id.txtView_meal_instruction);
+        txt_instruction.setMovementMethod(ScrollingMovementMethod.getInstance());
 
+        String detail = String.format("Meal: %s\n\nCategory: %s\n\nCuisine: %s",
+                getIntent().getStringExtra("meal"),
+                getIntent().getStringExtra("category"),
+                getIntent().getStringExtra("area"));
+
+        txt_detail.setText(detail);
+        txt_instruction.setText(getIntent().getStringExtra("instruction"));
 
     }
 
-    private Drawable getDrawableFromURL(String imagepath) {
-        Log.i(TAG, imagepath);
-        try {
-            Bitmap bmp;
-            HttpURLConnection connection = (HttpURLConnection) new URL(imagepath).openConnection();
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            bmp = BitmapFactory.decodeStream(input);
-            return new BitmapDrawable(Resources.getSystem(), bmp);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 }
--- app/src/main/java/edu/neu/madcourse/numad21fa_yuzou/WebServiceActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_yuzou/WebServiceActivity.java
@@ -1,6 +1,5 @@
 package edu.neu.madcourse.numad21fa_yuzou;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -10,8 +9,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +16,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -42,8 +41,11 @@ public class WebServiceActivity extends AppCompatActivity {
     private List<Meal> mealList;
     private TextView txtResult;
     private EditText edtInput;
+    private MviewAdapter mealAdapter;
     private ProgressBar progressBar;
-    private Handler myHandler;
+    private RadioGroup rp_searchType;
+    private boolean searchMeal;
+    private boolean searchCuisine;
 
 
     @Override
@@ -52,18 +54,36 @@ public class WebServiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_service);
 
         mealList = new ArrayList<>();
+        searchMeal = false;
+        searchCuisine = false;
 
         edtInput = findViewById(R.id.et_meal_search_input);
-
         txtResult = findViewById(R.id.txt_meal_result);
         txtResult.setText("");
-        ProgressBar pbSearch = findViewById(R.id.pb_mealsearch);
+        progressBar = findViewById(R.id.pb_mealsearch);
+        progressBar.setVisibility(View.GONE);
+        rp_searchType = findViewById(R.id.rgoup_meal_searchgroup);
         Button btnSearch = findViewById(R.id.btn_meal_search);
         btnSearch.setOnClickListener(new MyClickListener());
 
-        progressBar = findViewById(R.id.pb_mealsearch);
-        progressBar.setVisibility(View.GONE);
-
+        // check radio button group status.
+        rp_searchType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                // check search type.
+                RadioButton radioButton = findViewById(i);
+                switch (radioButton.getId()){
+                    case R.id.rb_search_meal:
+                        searchMeal = true;
+                        searchCuisine = false;
+                        break;
+                    case R.id.rb_search_area:
+                        searchCuisine = true;
+                        searchMeal = false;
+                        break;
+                }
+            }
+        });
     }
 
 
@@ -75,7 +95,7 @@ public class WebServiceActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
 
-            myHandler = new MyHandler();
+            progressBar.setVisibility(View.VISIBLE);
             // get user input as search keyword.
             editSearchUrl();
 
@@ -92,7 +112,7 @@ public class WebServiceActivity extends AppCompatActivity {
                                         dialogInterface.cancel();
                                     }
                                 })
-                        .setPositiveButton("Re-Enter",
+                        .setPositiveButton("OK",
                                 new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialogInterface, int i) {
@@ -101,50 +121,78 @@ public class WebServiceActivity extends AppCompatActivity {
                                 });
                 alert_nullInput.show();
             }
+            // alert user if search type is not selected.
+            else if (!searchMeal && !searchCuisine){
+                AlertDialog.Builder alert_nullInput = new AlertDialog.Builder(
+                        (WebServiceActivity.this));
+                alert_nullInput.setTitle("Invalid Input").setMessage("Select a search type.")
+                        .setCancelable(true)
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.cancel();
+                                    }
+                                })
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        dialogInterface.cancel();
+                                    }
+                                });
+                alert_nullInput.show();
+            }
+
             // search the database using keyword.
             else {
-
                 Thread searchThread = new Thread(new WebSearch());
                 searchThread.start();
-
                 // display result
                 if (mealList.size() == 0){
                     txtResult.setText("0 meal found.");
                 }
                 else {
+                    // create recycler view to hold result.
                     creatRecyclerView();
-                    txtResult.setText(String.format("%d meals found. Click a meal to see details.",
-                            mealList.size()));
+                    if (searchMeal){
+                        txtResult.setText(String.format("%d meals found. Click a meal to see details.",
+                                mealList.size()));
+                    }
+                    if (searchCuisine){
+                        txtResult.setText(String.format("%d meals found. Search meal name for detail.",
+                                mealList.size()));
+                    }
                 }
+
             }
-            progressBar.setVisibility(View.GONE);
-            Message message = new Message();
-            message.what = 2;
-            myHandler.sendMessage(message);
 
         }
     }
 
+    // edit url link according to user choice
     private void editSearchUrl() {
         String keyWord = edtInput.getText().toString();
         if (TextUtils.isEmpty(keyWord)) {
             urllink = null;
         }
         else {
-            urllink = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + keyWord;
+            if (searchMeal){
+                urllink = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + keyWord;
+            }
+            else if (searchCuisine){
+                urllink = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + keyWord;
+            }
         }
 
 ;    }
+
 
 
     private class WebSearch implements Runnable {
 
         @Override
         public void run() {
-
-            Message message = new Message();
-            message.what = 1;
-            myHandler.sendMessage(message);
 
             JSONArray jArray = new JSONArray();
             JSONObject jObject = new JSONObject();
@@ -174,6 +222,7 @@ public class WebServiceActivity extends AppCompatActivity {
                 // handle result
                 jObject = new JSONObject(resp);
                 jArray = jObject.getJSONArray("meals");
+                conn.disconnect();
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -192,10 +241,19 @@ public class WebServiceActivity extends AppCompatActivity {
                     JSONObject jmeal = jArray.getJSONObject(i);
                     String id = jmeal.getString("idMeal");
                     String name = jmeal.getString("strMeal");
-                    String category = jmeal.getString("strCategory");
-                    String area = jmeal.getString("strArea");
-                    String instruction = jmeal.getString("strInstructions");
                     String path = jmeal.getString("strMealThumb");
+                    String category = "";
+                    String area = "";
+                    String instruction = "";
+                    if (searchMeal){
+                        category = jmeal.getString("strCategory");
+                        area = jmeal.getString("strArea");
+                        instruction = jmeal.getString("strInstructions");
+                    }
+                    else if (searchCuisine){
+                        area = edtInput.getText().toString();
+                    }
+
                     Meal meal = new Meal(id, name, category, area, instruction, path);
                     mealList.add(meal);
                 }
@@ -203,17 +261,20 @@ public class WebServiceActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
+            progressBar.setVisibility(View.INVISIBLE);
         }
     }
 
 
+    /**
+     * Create recyclerview that holds the result.
+     */
     private void creatRecyclerView() {
         RecyclerView mealView = findViewById(R.id.recyclerview_meal);
-        MviewAdapter mealAdapter = new MviewAdapter(mealList);
+        mealAdapter = new MviewAdapter(mealList);
         mealView.setLayoutManager(new LinearLayoutManager(this));
         mealView.setItemAnimator(new DefaultItemAnimator());
         mealView.setAdapter(mealAdapter);
-
     }
 
     private class MviewAdapter extends RecyclerView.Adapter<MviewHolder>{
@@ -223,23 +284,21 @@ public class WebServiceActivity extends AppCompatActivity {
             this.list = list;
         }
 
-        @NonNull
         @Override
-        public MviewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public MviewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext()).inflate(
                     R.layout.meal_card, parent,false);
             return new MviewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull MviewHolder holder, int position) {
+        public void onBindViewHolder(MviewHolder holder, int position) {
             Meal card = (Meal) list.get(position);
             holder.mealName.setText(card.getMeal());
             holder.category.setText(card.getCategory());
             holder.area.setText(card.getArea());
-            holder.id = card.getId();
             holder.instruction = card.getInstruction();
-            holder.imagepath = card.getImagepath();
+            holder.id = card.getId();
         }
 
         @Override
@@ -252,44 +311,33 @@ public class WebServiceActivity extends AppCompatActivity {
         private TextView mealName;
         private TextView category;
         private TextView area;
-        private String id;
         private String instruction;
-        private String imagepath;
+        private String id;
 
-        public MviewHolder(@NonNull View itemView) {
+        public MviewHolder(View itemView) {
             super(itemView);
             mealName = itemView.findViewById(R.id.txt_mealinfo_meal);
             category = itemView.findViewById(R.id.txt_mealinfo_category);
             area = itemView.findViewById(R.id.txt_mealinfo_area);
+            // when click the meal item, open new activity to show meal details.
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Intent intent = new Intent(WebServiceActivity.this, MealDetailActivity.class);
-                    String mealDetail = String.format("Meal: %s\n\n%s\n\n%s\n\n\n",
-                            mealName.getText().toString(), category.getText().toString(),
-                            area.getText().toString());
-                    intent.putExtra("meal", mealDetail);
-                    intent.putExtra("path", imagepath);
-                    intent.putExtra("instruction", instruction);
-                    startActivity(intent);
+
+                    if (searchMeal){
+                        Intent intent = new Intent(WebServiceActivity.this, MealDetailActivity.class);
+                        intent.putExtra("id", id);
+                        intent.putExtra("meal", mealName.getText().toString());
+                        intent.putExtra("area", area.getText().toString());
+                        intent.putExtra("instruction", instruction);
+                        intent.putExtra("category", category.getText().toString());
+                        startActivity(intent);
+                    }
+
                 }
             });
         }
     }
-
-    private class MyHandler extends Handler {
-        @Override
-        public void handleMessage(@NonNull Message msg){
-            if (msg.what == 1) {
-                progressBar.setVisibility(View.VISIBLE);
-            }
-            else {
-                progressBar.setVisibility(View.GONE);
-            }
-        }
-    }
-
-
 
 
 }
